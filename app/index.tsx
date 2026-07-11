3c571027c0e5ff45a83274c5151a2ad7a151f12e
42zhrvn9n6-spec <42zhrvn9n6@privaterelay.appleid.com>
2026-07-12 00:25:10 +0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,5 @@
 import { Redirect } from 'expo-router';
-import { useAuthStore } from '../src/stores/authStore';
 
 export default function Index() {
-  const { isGuest } = useAuthStore();
-  return <Redirect href={isGuest ? "/(auth)/welcome" : "/(tabs)"} />;
+  return <Redirect href="/(auth)/onboarding" />;
 }
